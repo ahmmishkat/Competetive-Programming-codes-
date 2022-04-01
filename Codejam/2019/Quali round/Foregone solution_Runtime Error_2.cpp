b1296faa735cf13c45bfb54a7bdc4bb4ada2e2ed
ahmmishkat <74598986+ahmmishkat@users.noreply.github.com>
2022-04-01 16:37:10 +0600

    Create Foregone solution_Runtime Error_2.cpp

---
+++ Codejam/2019/Quali round/Foregone solution_Runtime Error_2.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void solve()
+{
+    string f,g;
+    cin>>f;
+    for (int i=0;i<f.length();i++)
+    {
+        g.insert(i,"a");
+    }
+    for (int i=f.length()-1;i>=0;i--)
+    {
+        if (f[i]=='4')
+        {
+            f[i]='2';
+            g[i]='2';
+        }
+         if (g[i]=='a')
+            g[i]='0';
+    }
+    ll int num1=stoi(f);
+    ll int num2=stoi(g);
+    cout<<num1<<' '<<num2<<" "<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    for (int i=1; i<=n; i++)
+    {
+        cout<<"Case #"<<i<<":"<<" ";
+        solve();
+    }
+}
